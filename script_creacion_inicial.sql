fda7293ac6537089e0814f0b7e7a3c48dd89d5fe
Julián Berbel <julianberbel@hotmail.com>
2017-05-09 01:38:30 -0300

    cambiados tipos de datos por de la tabla maestra

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -71,24 +71,24 @@ CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Funcionalidad_X_Rol(
 
 CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Usuario(
     usua_id INT IDENTITY PRIMARY KEY,
-    usua_user NVARCHAR(200) UNIQUE,
+    usua_user NVARCHAR(20) UNIQUE,
     usua_pass VARBINARY(200),
 	usua_habilitado BIT,
     usua_intentos TINYINT);
 
 CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Cliente(
     clie_id INT PRIMARY KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Usuario(usua_id),
-    clie_dni NVARCHAR(200),
-    clie_nombre NVARCHAR(200),
-	clie_apellido NVARCHAR(200),
+    clie_dni NUMERIC(18,0) UNIQUE,
+    clie_nombre VARCHAR(255),
+	clie_apellido VARCHAR(255),
 	clie_fecha_nacimiento DATETIME);
 
 CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Contacto(
     cont_id INT PRIMARY KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Cliente(clie_id),
-	cont_mail NVARCHAR(200),
-	cont_telefono NVARCHAR(200),
-	cont_domicilio NVARCHAR(200),
-	cont_codigo_postal NVARCHAR(20));
+	cont_mail NVARCHAR(255) UNIQUE,
+	cont_telefono NUMERIC(18,0),
+	cont_domicilio NVARCHAR(255),
+	cont_codigo_postal NUMERIC(18,0));
 
 CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Rol_X_Usuario(
     cod_rol TINYINT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Rol(rol_id),
@@ -97,15 +97,15 @@ CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Rol_X_Usuario(
 
 CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Turno(
     turn_id TINYINT IDENTITY PRIMARY KEY,
-	turn_hora_inicio Time(0),
-	turn_hora_fin Time(0),
-	turn_descripcion NVARCHAR(200),
+	turn_hora_inicio NUMERIC(18,0),
+	turn_hora_fin NUMERIC(18,0),
+	turn_descripcion NVARCHAR(255),
 	turn_valor_kilometro NUMERIC(10,2),
 	turn_precio_base NUMERIC(10,2),
 	turn_habilitado BIT);
 
 CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Rendicion(
-    rend_id INT IDENTITY PRIMARY KEY,
+    rend_id INT PRIMARY KEY,
     rend_turno TINYINT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Turno(turn_id),
 	rend_chofer INT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Usuario(usua_id),
 	rend_fecha DATETIME,
@@ -115,9 +115,11 @@ CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Vehiculo(
     vehi_id INT IDENTITY PRIMARY KEY,
     vehi_chofer INT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Usuario(usua_id),
 	vehi_turno TINYINT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Turno(turn_id),
-	vehi_patente NVARCHAR(7),
-	vehi_marca NVARCHAR(50),
-	vehi_modelo NVARCHAR(50),
+	vehi_patente VARCHAR(10),
+	vehi_licencia VARCHAR(26),
+	vehi_rodado VARCHAR(10),
+	vehi_marca VARCHAR(255),
+	vehi_modelo VARCHAR(255),
 	vehi_habilitado BIT);
 
 CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Viaje(
@@ -126,7 +128,7 @@ CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Viaje(
     viaj_vehiculo INT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Vehiculo(vehi_id),
 	viaj_cliente INT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Usuario(usua_id),
 	viaj_turno TINYINT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Turno(turn_id),
-	viaj_kms NUMERIC(10,2),
+	viaj_kms NUMERIC(18,0),
     viaj_inicio DATETIME,
     viaj_fin DATETIME);
 
@@ -135,7 +137,7 @@ CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Factura(
     fact_cliente INT NOT NULL FOREIGN KEY REFERENCES LOS_MODERADAMENTE_ADECUADOS.Usuario(usua_id),
     fact_fecha_inicio DATETIME,
     fact_fecha_fin DATETIME,
-    fact_importe_total NUMERIC(10,2));
+    fact_importe_total NUMERIC(18,2));
 
 CREATE TABLE LOS_MODERADAMENTE_ADECUADOS.Item_Factura(
     item_id INT IDENTITY PRIMARY KEY,
@@ -167,19 +169,31 @@ VALUES	('ABM de Rol'),
 --Funcionalidad_X_Rol
 
 INSERT [LOS_MODERADAMENTE_ADECUADOS].Funcionalidad_X_Rol (cod_func, cod_rol)
-VALUES (1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (6, 3), (7, 3), (8, 3), (9, 3)
+	VALUES (1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (6, 3), (7, 3), (8, 3), (9, 3)
 
 --Usuario
 
 INSERT [LOS_MODERADAMENTE_ADECUADOS].Usuario (usua_user, usua_pass, usua_habilitado, usua_intentos)
-VALUES ('admin', HASHBYTES('SHA256', 'w23e'), 1, 0)
+	VALUES ('admin', HASHBYTES('SHA256', 'w23e'), 1, 0)
 
-/*INSERT [LOS_MODERADAMENTE_ADECUADOS].Usuario (usua_user, usua_pass, usua_habilitado, usua_intentos)
-select [Chofer_Nombre], [Chofer_Apellido], 1, 0
-from [gd_esquema].Maestra*/
+INSERT [LOS_MODERADAMENTE_ADECUADOS].Usuario (usua_user, usua_pass, usua_habilitado, usua_intentos)
+	SELECT CONVERT(varchar(20), Chofer_Dni), HASHBYTES('SHA256', CONVERT(varchar(20), Chofer_Dni)), 1, 0
+	FROM [gd_esquema].Maestra
+
+INSERT [LOS_MODERADAMENTE_ADECUADOS].Usuario (usua_user, usua_pass, usua_habilitado, usua_intentos)
+	SELECT CONVERT(varchar(20), Cliente_Dni), HASHBYTES('SHA256', CONVERT(varchar(20), Cliente_Dni)), 1, 0
+	FROM [gd_esquema].Maestra
 
 --Cliente
-
+/*
+INSERT [LOS_MODERADAMENTE_ADECUADOS].Cliente (clie_dni, clie_nombre, clie_apellido, clie_fecha_nacimiento)
+	SELECT Cliente_Dni, Cliente_Nombre, Cliente_Apellido, Cliente_Fecha_Nac
+	FROM [gd_esquema].Maestra
+	
+INSERT [LOS_MODERADAMENTE_ADECUADOS].Cliente (clie_dni, clie_nombre, clie_apellido, clie_fecha_nacimiento)
+	SELECT Chofer_Dni, Chofer_Nombre, Chofer_Apellido, Chofer_Fecha_Nac
+	FROM [gd_esquema].Maestra
+*/
 --Contacto
 
 --Rol_X_Usuario
